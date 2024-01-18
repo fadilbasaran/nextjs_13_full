2c9d73f8e3fd99be95a9eddba5b10c14eb1a481c
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-18 21:59:48 +0300

    Update

--- netfilms-course/app/layout.js
+++ netfilms-course/app/layout.js
@@ -3,7 +3,7 @@ import '@/styles/global.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body>{children}</body>
+      <body className="container">{children}</body>
     </html>
   )
 }
