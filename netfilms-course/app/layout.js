d292cc6eb7288ca8a8f14863c4e6c36a35c3f9be
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-31 17:22:22 +0300

    Net-films project deleted on the courses

--- netfilms-course/app/layout.js
+++
@@ -1,9 +0,0 @@
-import '@/styles/reset.css';
-import '@/styles/global.css';
-export default function RootLayout({ children }) {
-  return (
-    <html lang="tr">
-      <body className="container">{children}</body>
-    </html>
-  )
-}
